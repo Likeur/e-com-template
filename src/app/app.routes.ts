6bae96ec818e1df2dd33eb0c092c148f0f15bfe8
Likeur <jacksimba28@gmail.com>
2025-01-11 22:30:57 +0200

    adding tailwindcss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
         path:'',
         title:'home',
         loadComponent: () => import("./layout/home/home.component")
+    },
+    {
+        path:'**',
+        loadComponent: () => import("./components/notfound/notfound.component")
     }
 ];
