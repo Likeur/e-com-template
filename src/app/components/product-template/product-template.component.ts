e0cded10fc737d4852a1d976a3fb45248c6e7aec
Likeur <jacksimba28@gmail.com>
2025-01-13 16:04:55 +0200

    adding productlist and product template

---
+++ src/app/components/product-template/product-template.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-template',
+  imports: [],
+  templateUrl: './product-template.component.html',
+  styles: ``
+})
+export class ProductTemplateComponent {
+
+}
---
+++ src/app/components/productlist/productlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productlist',
+  imports: [],
+  templateUrl: './productlist.component.html',
+  styles: ``
+})
+export class ProductlistComponent {
+
+}
