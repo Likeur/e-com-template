35afa0b574b0840074e8009809a89f6e068cfd51
Likeur <jacksimba28@gmail.com>
2025-01-13 11:57:56 +0200

    refactoring navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path:'home',
-        title:'home',
+        title:'Ecommerce template',
         loadComponent: () => import("./layout/home/home.component")
     },
     {
---
+++ src/app/components/herosection/herosection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-herosection',
+  imports: [],
+  templateUrl: './herosection.component.html',
+  styles: ``
+})
+export class HerosectionComponent {
+
+}
