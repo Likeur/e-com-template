a74fee58a09c2870dbadd1a3aee3f2a10ca826bb
Likeur <jacksimba28@gmail.com>
2025-01-15 16:33:49 +0200

    working on productlist and productlist template

--- src/app/components/productlist/productlist.component.ts
+++ src/app/components/productlist/productlist.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductTemplateComponent } from '../product-template/product-template.component';
 
 @Component({
   selector: 'app-productlist',
-  imports: [],
+  imports: [ProductTemplateComponent],
   templateUrl: './productlist.component.html',
   styles: ``
 })
--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HerosectionComponent } from '../../components/herosection/herosection.component';
 import { NavComponent } from '../../components/nav/nav.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ProductlistComponent } from '../../components/productlist/productlist.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HerosectionComponent,NavComponent, FooterComponent],
+  imports: [HerosectionComponent, ProductlistComponent,NavComponent, FooterComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
