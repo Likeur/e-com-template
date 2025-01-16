8001aaabfc5507453f35787b3be4c34d7b91e883
Likeur <jacksimba28@gmail.com>
2025-01-16 20:48:43 +0200

    fixe dark and light mode

--- src/app/components/productdetails/productdetails.component.ts
+++ src/app/components/productdetails/productdetails.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-productdetails',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './productdetails.component.html',
   styles: ``
 })
