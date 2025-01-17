c71b6076161bf45c623cc9914b0cbbe2cc109916
Likeur <jacksimba28@gmail.com>
2025-01-17 20:57:32 +0200

    adding toggle animation on open/close cart

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CartlistComponent } from '../cartlist/cartlist.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterModule, CartlistComponent],
+  imports: [RouterModule, CartlistComponent, CommonModule],
   templateUrl: './nav.component.html',
   styles: ``
 })
 export class NavComponent {
 
+  public isCartOpen = false
+
+  public togglecart(){
+    this.isCartOpen = !this.isCartOpen
+  }
 }
